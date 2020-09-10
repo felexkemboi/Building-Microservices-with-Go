a122bcaa65bfb6c1f249629e83be7649836ab13f
felexkemboi <felokemboi10@gmail.com>
2020-09-10 17:09:17 +0300

    episode 3

--- main.go
+++ main.go
@@ -1,55 +1,62 @@
 package main
 
-import ("net/http"
+import (
 	"context"
-		 "time"
-		 "os/signal"
-		 "log"
-		"github.com/nicholasjackson/building-microservices-youtube/product-api/handlers"
-		"os"
-		)
-func main (){
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	"github.com/nicholasjackson/building-microservices-youtube/product-api/handlers"
+	"github.com/nicholasjackson/env"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
+func main() {
+
+	env.Parse()
+
+	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
+	sm.Handle("/", ph)
 
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
-
-	s := &http.Server{
-		Addr : ":6060",                     //configure the bind address
-		Handler: sm,                        //set the default handler
-		ErrorLog : l,						// set the logger for the server
-		IdleTimeout:120*time.Second,        // 
-		ReadTimeout:5*time.Second,
-		WriteTimeout:10*time.Second,
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
-			l.Fatal(err)
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
-	} ()
+	}()
 
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan,os.Kill)
+	// trap sigterm or interupt and gracefully shutdown the server
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
-	sig := <-sigChan
-	l.Println("Recieved terminate", sig)
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 
-
-	s.ListenAndServe()
-
-	tc,cancel := context.WithTimeout(context.Background(), 30*time.Second)
-
-	defer cancel()
-
-	//tc, _ :=  context.WithTimeout(context.Background(), 30*time.Second)
-
-	s.Shutdown(tc)
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
-
